7eca3e61489db60949f7ff3edd62c95185c4e3c3
Demur Rumed <gunkmute@gmail.com>
2015-10-24 23:56:07 +0000

    Grow a little to shrink a little; VLA over malloc

--- sxi.c
+++ sxi.c
@@ -7,19 +7,20 @@ int main(int argc, char **argv){
 	signal(SIGUSR1, SIG_IGN);
 	const char *const home = getenv("HOME");
 	const int homelen = strlen(home);
-	char *const xrc[2] = {malloc(homelen+strlen("/.xserverrc")+1), 0};
-	memcpy(*xrc, home, homelen);
-	memcpy(*xrc+homelen, "/.xserverrc", strlen("/.xserverrc")+1);
+	char xrcpath[homelen+strlen("/.xserverrc")+1];
+	char *const xrc[2] = {xrcpath, 0};
+	memcpy(xrcpath, home, homelen);
+	memcpy(xrcpath+homelen, "/.xserverrc", strlen("/.xserverrc")+1);
 	const pid_t serverpid = fork();
 	if (!serverpid) {
 		signal(SIGTTIN, SIG_IGN);
 		signal(SIGTTOU, SIG_IGN);
-		return setpgid(0, getpid()) || execvp(*xrc, xrc);
+		return setpgid(0, getpid()) || execvp(xrcpath, xrc);
 	}else if (serverpid < 0) return fputs("server fork failed", stderr);
 	const pid_t clientpid = fork();
 	if (!clientpid){
-		memcpy(*xrc+homelen+3, "initrc", strlen("initrc")+1);
-		return setuid(getuid()) || setpgid(0, getpid()) || execvp(*xrc, xrc);
+		memcpy(xrcpath+homelen+3, "initrc", strlen("initrc")+1);
+		return setuid(getuid()) || setpgid(0, getpid()) || execvp(xrcpath, xrc);
 	} else if (clientpid < 0) return fputs("client fork failed", stderr);
 	pid_t pid;
 	do pid = wait(0); while (pid != clientpid && pid != serverpid);
