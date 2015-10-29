390da90dff5442c0f0fb432175237880efcade5a
Demur Rumed <gunkmute@gmail.com>
2015-10-29 02:08:29 +0000

    I give; sigpending busyloop

--- sxi.c
+++ sxi.c
@@ -4,7 +4,10 @@
 #include <sys/wait.h>
 #include <unistd.h>
 int main(int argc, char **argv){
-	signal(SIGUSR1, SIG_IGN);
+	sigset_t sset, pset;
+	sigemptyset(&sset);
+	sigaddset(&sset, SIGUSR1);
+	sigprocmask(SIG_BLOCK, &sset, 0);
 	const char *const home = getenv("HOME");
 	const int homelen = strlen(home);
 	char xrcpath[homelen+strlen("/.xserverrc")+1];
@@ -13,10 +16,13 @@ int main(int argc, char **argv){
 	memcpy(xrcpath+homelen, "/.xserverrc", strlen("/.xserverrc")+1);
 	const pid_t serverpid = fork();
 	if (!serverpid) {
+		signal(SIGUSR1, SIG_IGN);
 		signal(SIGTTIN, SIG_IGN);
 		signal(SIGTTOU, SIG_IGN);
 		return setpgid(0, getpid()) || execvp(xrcpath, xrc);
 	}else if (serverpid < 0) return fputs("server fork failed", stderr);
+	sigemptyset(&pset);
+	do sigpending(&pset); while (!sigismember(&pset, SIGUSR1));
 	const pid_t clientpid = fork();
 	if (!clientpid){
 		memcpy(xrcpath+homelen+3, "initrc", strlen("initrc")+1);
