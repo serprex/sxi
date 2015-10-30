41f651423ff767ea95cd377add8add29fcf8e704
Demur Rumed <gunkmute@gmail.com>
2015-10-30 00:04:24 +0000

    Don't ign TTOU/TTIN, be a vfork ricer, reuse sset

--- sxi.c
+++ sxi.c
@@ -4,7 +4,7 @@
 #include <sys/wait.h>
 #include <unistd.h>
 int main(int argc, char **argv){
-	sigset_t sset, pset;
+	sigset_t sset;
 	sigemptyset(&sset);
 	sigaddset(&sset, SIGUSR1);
 	sigprocmask(SIG_BLOCK, &sset, 0);
@@ -14,20 +14,16 @@ int main(int argc, char **argv){
 	char *const xrc[2] = {xrcpath, 0};
 	memcpy(xrcpath, home, homelen);
 	memcpy(xrcpath+homelen, "/.xserverrc", strlen("/.xserverrc")+1);
-	const pid_t serverpid = fork();
+	const pid_t serverpid = vfork();
 	if (!serverpid) {
 		signal(SIGUSR1, SIG_IGN);
-		signal(SIGTTIN, SIG_IGN);
-		signal(SIGTTOU, SIG_IGN);
-		return setpgid(0, getpid()) || execvp(xrcpath, xrc);
+		_exit(setpgid(0, getpid()) || execvp(xrcpath, xrc));
 	}else if (serverpid < 0) return fputs("server fork failed", stderr);
-	sigemptyset(&pset);
-	do sigpending(&pset); while (!sigismember(&pset, SIGUSR1));
-	const pid_t clientpid = fork();
-	if (!clientpid){
-		memcpy(xrcpath+homelen+3, "initrc", strlen("initrc")+1);
-		return setuid(getuid()) || setpgid(0, getpid()) || execvp(xrcpath, xrc);
-	} else if (clientpid < 0) return fputs("client fork failed", stderr);
+	memcpy(xrcpath+homelen+3, "initrc", strlen("initrc")+1);
+	do sigpending(&sset); while (!sigismember(&sset, SIGUSR1));
+	const pid_t clientpid = vfork();
+	if (!clientpid) _exit(setuid(getuid()) || setpgid(0, getpid()) || execvp(xrcpath, xrc));
+	else if (clientpid < 0) return fputs("client fork failed", stderr);
 	pid_t pid;
 	do pid = wait(0); while (pid != clientpid && pid != serverpid);
 	killpg(clientpid, SIGHUP);
